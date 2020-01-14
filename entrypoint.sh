084ee112571f919a15a3b9744a07da2ebb7ed484
bgsandan <39411049+bgsandan@users.noreply.github.com>
2020-01-14 11:59:06 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 echo "Building Sass"
+npm ci
 /node_modules/.bin/node-sass --recursive --output css --source-map true --source-map-contents scss
 echo "Running eleventy"
 eleventy $INPUT_ARGS
